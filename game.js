1e6ac120b4cf0ad62f59000d80b0d255119ae70c
glenroyalex <162653654+glenroyalex@users.noreply.github.com>
2026-06-03 14:06:25 +1000

    Create game.js

---
+++ game.js
@@ -0,0 +1,21 @@
+kaboom({
+  background: [0, 0, 0],
+  scale: 2,
+});
+
+loadSprite("player", "assets/player.png");
+
+scene("main", () => {
+  const player = add([
+    sprite("player"),
+    pos(80, 80),
+    area(),
+  ]);
+
+  onKeyDown("left", () => player.move(-120, 0));
+  onKeyDown("right", () => player.move(120, 0));
+  onKeyDown("up", () => player.move(0, -120));
+  onKeyDown("down", () => player.move(0, 120));
+});
+
+go("main");
